7964a3d66d34c95717128642b8c854ae66a176b5
Debbie MidstCodes <trentbuzzmedia@gmail.com>
2026-08-05 09:12:55 +0000

    feat(utils): add classnames re-export for simpler cn utility imports

---
+++ src/lib/utils/classnames.ts
@@ -0,0 +1,6 @@
+/**
+ * Re-export of the cn utility for consistent import paths.
+ * Components can import from @/lib/utils/classnames instead of @/lib/utils
+ * when they only need the className merge utility.
+ */
+export { cn } from "@/lib/utils";
